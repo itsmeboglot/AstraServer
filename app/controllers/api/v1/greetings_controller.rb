a162f8bd1d87139176aa600894a7cbc14723dd39
Bohdan Lotysh <lotbod@gmail.com>
2024-02-29 17:54:16 +0000

    [MOD] routes.rb

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,11 @@
-class GreetingsController < ApplicationController
-  def hello
-    render plain: "HELLO PIDARAS BEZROBITNIY! Spectre apruvnuly?"
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def hello
+        render plain: "HELLO PIDARAS BEZROBITNIY! Spectre apruvnuly?"
+      end
+    end
   end
 end
