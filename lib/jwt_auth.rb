09ac7db0da7504cd5b7efb969c05a5b59df6b4ae
Bohdan Lotysh <lotbod@gmail.com>
2024-03-07 14:47:03 +0000

    [MOD] idk 5 maybe final 2

--- lib/jwt_auth.rb
+++ lib/jwt_auth.rb
@@ -1,7 +1,7 @@
 require 'jwt'
 
 module JwtAuth
-  SECRET_KEY = Rails.application.secret_key_base #ENV['SECRET_KEY_BASE']
+  SECRET_KEY = ENV['SECRET_KEY_BASE']
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
@@ -9,6 +9,7 @@ module JwtAuth
   end
 
   def self.decode(token)
+    puts("SECRET_KEY: #{SECRET_KEY}")
     decoded_token = JWT.decode(token, SECRET_KEY)[0]
     HashWithIndifferentAccess.new decoded_token
   end
